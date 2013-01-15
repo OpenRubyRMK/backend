ab38b9596d444f719d96f76dec6e09fb7e488207
Quintus <quintus@quintilianus.eu>
2013-01-15 21:52:57 +0100

    Pass the project root directory to the engine and add a nice quote on crash.
    Guess where the quote was taken from!

--- data/skeleton/bin/start.rb
+++ data/skeleton/bin/start.rb
@@ -1,21 +1,28 @@
 #!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
 # start.rb - OpenRubyRMK game startup file.
 # This is the first file executed when a game
 # starts up.
 
-# Require our dependencies
+# Require our standard library dependencies.
 require "bundler"
 require "pathname"
+
+# Require our non-standard dependencies from the Gemfile.
 Bundler.require(:default)
+
+# Find the project root, i.e. the directory
+# one level above this very file.
+root = Pathname.new(__FILE__).dirname.parent.expand_path
 
 # Start the game, and if it crashes, dump out an error
 # report.
 begin
-  engine = OpenRubyRMK::Engine.new(ARGV)
+  engine = OpenRubyRMK::Engine.new(ARGV, root)
   engine.start!
 rescue => e
-  path = Pathname.new(__FILE__).dirname.expand_path + "crashdump.log"
-  path.open("w") do |file|
+  crashlog = root + "crashdump.log"
+  crashlog.open("w") do |file|
     file.puts("OpenRubyRMK game crashdump from #{Time.now.strftime('%Y-%m-%d %H:%M:%S')}")
     file.puts(RUBY_DESCRIPTION)
     #file.puts("Engine version is #{OpenRubyRMK::Engine.version}.") # Not yet implemented
@@ -25,8 +32,15 @@ rescue => e
   end
 
   # Reraise and crash
-  $stderr.puts("==== CRASH ===")
-  $stderr.puts("Crash! Error information is below and can also be found in the file #{path}.")
+  $stderr.puts
+  $stderr.puts("-" * 80)
+  $stderr.puts '"It is an incident again, Captain, is it?"'
+  $stderr.puts '"No, professor. This is an accident, no incident."'
+  # TODO: Actually research that quote in my library. I’ve
+  # recalled it from memory.
+
+  $stderr.puts
+  $stderr.puts("Crashdump is here: #{crashlog}")
   $stderr.puts("If you think you found a bug, please report it at http://pegasus-alpha.eu/projects/openrubyrmk.")
   $stderr.puts
   raise
