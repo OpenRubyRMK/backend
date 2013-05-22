b02adf2511829df40bfffd8609faedf4c32c2f5e
Quintus <quintus@quintilianus.eu>
2013-05-22 14:30:23 +0200

    Add missing TSX file

---
+++ data/skeleton/data/tilesets/lpc.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="lpc" tilewidth="32" tileheight="32">
+  <image source="../resources/graphics/tilesets/lpc.png" width="288" height="1280"/>
+</tileset>
